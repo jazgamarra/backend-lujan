4d57002dbce4aa26c3b8c93a4b40ab39de94c4b0
jazgamarra <j444min@gmail.com>
2023-02-07 22:49:15 -0300

    jejeje

--- app.py
+++ app.py
@@ -3,16 +3,18 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 db = SQLAlchemy(app)
-id_usuario_actual = None 
+
+# Variable global para guardar el usuario actual de la sesion 
 usuario_actual = None 
 
 # Configurar la base de datos 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
 
 # Crear el modelo de tabla en la base de datos 
 class Paciente (db.Model):
     id = db.Column(db.Integer, primary_key = True)
-    cedula = db.Column(db.Integer, unique = True)
+    cedula = db.Column(db.Integer)
     nombre = db.Column(db.String(100))
     apellido = db.Column(db.String(100))
     tipo_de_sangre = db.Column(db.String(10))
@@ -37,14 +39,14 @@ class Paciente (db.Model):
         self.contacto_parentesco = contacto_parentesco
         self.password = password
 
-
 @app.route('/')
 def home():
     return render_template('home.html')
 
-@app.route('/index')
-def index():
-    return render_template('index.html', id_usuario_actual=id_usuario_actual, usuario_actual=usuario_actual) 
+@app.route('/ficha_paciente',  methods=['GET', 'POST'])
+def ficha_paciente():
+    global usuario_actual 
+    return render_template('ficha_paciente.html', usuario_actual=usuario_actual) 
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
@@ -59,9 +61,10 @@ def login():
         # Verificar si el usuario existe y la contraseña es correcta
         if paciente is not None: 
             if paciente.password == password:
-                id_usuario_actual = cedula 
+                global usuario_actual 
                 usuario_actual = paciente.__dict__
-                return render_template('index.html', id_usuario_actual=id_usuario_actual, usuario_actual=usuario_actual)
+                print(f"INICIO SESION EL USUARIO {usuario_actual}")
+                return render_template('ficha_paciente.html', usuario_actual=usuario_actual)
             else:
                 return 'La contrasenha es incorrecta. Intente de nuevo' 
         else:
@@ -71,6 +74,7 @@ def login():
 
 @app.route('/register', methods=['GET', 'POST'])
 def register():
+    global usuario_actual 
     if request.method == 'POST':
         cedula = request.form['cedula']
         nombre = request.form['nombre']
@@ -86,18 +90,67 @@ def register():
         paciente = Paciente(cedula, nombre, apellido, tipo_de_sangre, alergia, seguro_medico, contacto_nombre, contacto_telefono, contacto_parentesco, password)
         db.session.add(paciente)
         db.session.commit()
-        id_usuario_actual = cedula 
+
+        # Modificamos la variable global
         usuario_actual = paciente.__dict__
-        return render_template('index.html', id_usuario_actual=id_usuario_actual, usuario_actual=usuario_actual)
-    return render_template('register.html')
+        print(f"SE REGISTRO EL USUARIO {usuario_actual}")
+        return render_template('ficha_paciente.html', usuario_actual=usuario_actual)
+    return render_template('register.html', usuario_actual=usuario_actual)
 
+@app.route('/editar_ficha', methods=['GET', 'POST'])
+def editar_ficha(): 
+    if request.method == 'POST': 
+        # Guardamos los datos del formulario 
+        campo = request.form['campo']
+        nuevo_valor = request.form['valor']
 
+        # Buscamos el usuario en la base de datos
+        global usuario_actual # aclaramos que nos referimos a la variable global
+        paciente = Paciente.query.filter_by(cedula=usuario_actual['cedula']).first()
 
-@app.route('/ficha_paciente')
-def ficha_paciente(): 
-    return render_template('ficha_paciente.html')
+        # Editamos el campo que se haya seleccionado
+        if campo == 'nombre':
+            paciente.nombre = nuevo_valor
+        elif campo == 'apellido':
+            paciente.apellido = nuevo_valor
+        elif campo == 'tipo_de_sangre':
+            paciente.tipo_de_sangre = nuevo_valor
+        elif campo == 'alergia':
+            paciente.alergia = nuevo_valor
+        elif campo == 'seguro_medico':
+            paciente.seguro_medico = nuevo_valor
+        elif campo == 'contacto_nombre':
+            paciente.contacto_nombre = nuevo_valor
+        elif campo == 'contacto_telefono':
+            paciente.contacto_telefono = nuevo_valor
+        elif campo == 'contacto_parentesco':
+            paciente.contacto_parentesco = nuevo_valor
+        elif campo == 'password':
+            paciente.password = nuevo_valor
+        elif campo == 'cedula':
+            paciente.cedula = nuevo_valor 
 
+        # Guardamos los cambios en la base de datos
+        db.session.commit()
 
+        # Actualizamos la variable usuario_actual
+        usuario_actual = paciente.__dict__
+        return render_template('ficha_paciente.html', usuario_actual=usuario_actual)
+    return render_template('editar_ficha.html')
+
+@app.route('/logout')
+def logout():
+    global usuario_actual
+    usuario_actual = None
+    return render_template('home.html') 
+
+@app.route('/borrar/<id>')
+def borrar(id):
+    paciente = Paciente.query.filter_by(id=id).first()
+    db.session.delete(paciente)
+    db.session.commit()
+    return f'<h1> El paciente {id} se borro correctamente </h1>'
+    
 if __name__ == '__main__':
     db.create_all()
     app.run(debug=True) 
